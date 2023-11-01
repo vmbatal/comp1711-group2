d85cf30f9ba4ff8971b4e5b159a9fffb6f927869
Victor Manuel Batalla Plada <146420641+vmbatal@users.noreply.github.com>
2023-11-01 12:25:24 +0000

    /yesssssss

--- task1/StepsTask1.c
+++ task1/StepsTask1.c
@@ -2,26 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Define an appropriate struct
+#define MAX_LINE_SIZE 1000
+
 typedef struct {
 	char date[11];
 	char time[6];
 	int steps;
 } FITNESS_DATA;
 
-// Define any additional variables here
-
-
-
-// This is your helper function. Do not change it in any way.
-// Inputs: character array representing a row; the delimiter character
-// Ouputs: date character array; time character array; steps character array
 void tokeniseRecord(const char *input, const char *delimiter,
                     char *date, char *time, char *steps) {
-    // Create a copy of the input string as strtok modifies the string
     char *inputCopy = strdup(input);
     
-    // Tokenize the copied string
     char *token = strtok(inputCopy, delimiter);
     if (token != NULL) {        strcpy(date, token);
     }
@@ -36,16 +28,12 @@ void tokeniseRecord(const char *input, const char *delimiter,
         strcpy(steps, token);
     }
     
-    // Free the duplicated string
     free(inputCopy);
-
 }
 
-// Complete the main function
+int main(){
 
-int main() {
-
-    char* filename = "FitnessData_2023.csv";
+char* filename = "FitnessData_2023.csv";
     FILE *file = fopen(filename, "r");
     if (file == NULL) {
         printf("Error opening file\n");
@@ -55,18 +43,7 @@ int main() {
     int buffer_size = 999999;
     char line_buffer[buffer_size];
 
-    
-    //int num = 10, i;
-    
-    
-    //FITNESS_DATA fit []= {fscanf(file, "%d", num)};
-
-    //for(int i = 0; i < num; i++){
-        
-        //printf("%s %s %d\n", fit[i].date, fit[i].time, fit[i].steps);
-    //} 
-
-    int lin = 0;
+  int lin = 0;
     char rec;
 
     while (rec != EOF){
@@ -74,56 +51,48 @@ int main() {
         if (rec == '\n'){
             lin++;
         } 
-    }
+    }  
 
     printf("Number of records in file: %d\n", lin);
+    fclose(file);
+    fopen(filename, "r");
 
-    FITNESS_DATA fit[90];
+int lnum = 0;
+char line[MAX_LINE_SIZE+1];
 
-int records = 10;
+  while (1) {
+    fgets(line, MAX_LINE_SIZE,  file);
+    if (feof(file))  
+      break;
+    lnum++;
+    
+    const char coma[4] = ",";
+    char* tok;
+ 
+    tok = strtok(line, coma);
 
-char rd[60] = fscanf(file,"%s,%[^,],%d,\n",
-fit[records].date, fit[records].time, &fit[records].steps);
+    while (lnum<4){
+        while (tok != 0) {
+            printf("%s/", tok);
+            tok = strtok(0, coma);
+            break;
+        }
 
+        while (tok != 0) {
+            printf("%s/", tok);
+            tok = strtok(0, coma);
+            break;
+        }
 
-
-for (int i = 0; i<records; i++){
-    printf("%s/%s/%d",
-    fit[i].date, fit[i].time, fit[i].steps);
+        while (tok != 0) {
+            printf("%s", tok);
+            tok = strtok(0, coma);
+            break;
+        }
+    break;
+    }
 }
 
-    //while (lin = 1){
-       // rec = fgetc(file);
-        //if (rec == '\n'){
-          //  lin++;
-       // } 
-    // }
-    
-    //int linea = 0;
-    //int dato;
-    
-    //while ((dato = fgetc(file)) != EOF && linea <3){
-        //if (dato == '\n') linea++;
-        //putchar(dato);
-    //}
-    
-    
-    
-    //struct FITNESS_DATA fit
-
-    //int lines=3,i;
-    //FITNESS_DATA fit [] = {dato};
-    //for (i = 0; i < lines; i++){
-    //printf("%s/", fit[i].date);
-    //printf("%s/", fit[i].time);
-    //printf("%d/", fit[i].steps);
-    //}
-    
-    //strcpy(fit.date, "124");
-    //strcpy(fit.time, "132");
-    //printf("%s/", fit.date);
-    //printf("%s/", fit.time);
-
-    fclose(file);
-    return 0;
+  fclose(file);
+return 0;
 }
