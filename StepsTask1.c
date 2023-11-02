7bae6a018b6a860521ae68fce955c33e7fa54a07
Victor Manuel Batalla Plada <146420641+vmbatal@users.noreply.github.com>
2023-11-02 16:17:12 +0000

    /sdad

--- StepsTask1.c
+++ StepsTask1.c
@@ -2,26 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Define an appropriate struct
 typedef struct {
 	char date[11];
 	char time[6];
 	int steps;
 } FITNESS_DATA;
 
-// Define any additional variables here
-
-
-
-// This is your helper function. Do not change it in any way.
-// Inputs: character array representing a row; the delimiter character
-// Ouputs: date character array; time character array; steps character array
 void tokeniseRecord(const char *input, const char *delimiter,
                     char *date, char *time, char *steps) {
-    // Create a copy of the input string as strtok modifies the string
     char *inputCopy = strdup(input);
     
-    // Tokenize the copied string
     char *token = strtok(inputCopy, delimiter);
     if (token != NULL) {        strcpy(date, token);
     }
@@ -36,21 +26,65 @@ void tokeniseRecord(const char *input, const char *delimiter,
         strcpy(steps, token);
     }
     
-    // Free the duplicated string
     free(inputCopy);
-
 }
 
-// Complete the main function
-int main() {
-    char* filename = "data.txt";
-    FILE *file = open_file(filename, "r");
-    int buffer_size = 100;
-    char line_buffer[buffer_size];
-    while (fgets(line_buffer, buffer_size, file) != NULL)
-    {
-        printf("%s", line_buffer);
+int main(){
+
+char* filename = "FitnessData_2023.csv";
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("Error opening file\n");
+        return 1;
     }
+
+    int lin = 0;
+    char rec;
+
+    while (rec != EOF){
+        rec = fgetc(file);
+        if (rec == '\n'){
+            lin++;
+        } 
+    }  
+
+    printf("Number of records in file: %d\n", lin);
     fclose(file);
-    return 0;
+    fopen(filename, "r");
+
+    int lnum = 0;
+    char line[80];
+    const char coma[4] = ",";
+    char* info;
+
+    while (1) {
+        fgets(line, 80,  file);
+        if (feof(file))
+            break;
+            lnum++;
+            info = strtok(line,coma);
+
+        while (lnum<4){
+            while (info != 0) {
+                printf("%s/", info);
+                info = strtok(0, coma);
+                break;
+                }
+
+            while (info != 0) {
+                printf("%s/", info);
+                info = strtok(0, coma);
+                break;
+                }
+
+            while (info != 0) {
+                printf("%s", info);
+                info = strtok(0, coma);
+                break;
+                }
+    break;
+    }}
+
+fclose(file);
+return 0;
 }
