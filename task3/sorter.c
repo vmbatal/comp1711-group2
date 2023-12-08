e9e1d8f4cc5deb735d46cce7f17f27632fd08ca0
Victor Manuel Batalla Plada <146420641+vmbatal@users.noreply.github.com>
2023-12-08 22:01:33 +0000

    /dkh

--- task3/sorter.c
+++ task3/sorter.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "sort.h"
+#include "FitnessDataStruct.h"
 
 void tokeniseRecord(const char *input, const char *delimiter, char *date, char *time, char *steps) {
 
